c594ce1b94717ec396bdd3b852f20b3c27d06494
Rutvi Malaviya <60802831+Rutvi-Malaviya@users.noreply.github.com>
2020-09-22 15:53:48 +0530

    Add files via upload

---
+++ speed.js
@@ -0,0 +1,75 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originalText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+var timer=[0,0,0,0];
+var interval;
+var timerRunnning = false;
+
+function addLeadingZero(time){
+    if(time <= 9){
+        time = "0" + time;
+    }
+
+    return time;
+}
+
+// Run the timer
+function runTimer(){
+    let currentTime = addLeadingZero(timer[0]) + ":" + addLeadingZero(timer[1]) + ":" + addLeadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100)/60)
+    timer[1] = Math.floor((timer[3]/100) - (timer[0]*60));
+    timer[2] = Math.floor(timer[3]-timer[1]*100 - timer[0]*6000);
+}
+
+
+// Start the timer
+function startTimer(){
+    let testTextLength = testArea.value.length;
+    if(testTextLength===0 && !timerRunnning){
+        timerRunnning = true;
+        interval = setInterval( runTimer, 10);
+    }
+
+}
+
+// check the content of test area
+function spellCheck(){
+    let testTextEntered = testArea.value;
+    let originalTextMatch = originalText.substring(0,testTextEntered.length);
+    
+    if(testTextEntered==originalText){
+        clearInterval(interval);
+        testWrapper.style.borderColor = "#429890"; // green
+    }
+    else{
+        if(testTextEntered==originalTextMatch){
+            testWrapper.style.borderColor = "#65CCf3"; // blue
+        }
+        else{
+            testWrapper.style.borderColor = "#E95D0F"; // red
+        }
+    }
+}
+
+// reset the test area
+function reset(){
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunnning = false;
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
+}
+
+// Event listener for keyboard input and reset button
+testArea.addEventListener("keypress",startTimer, false);
+testArea.addEventListener("keyup",spellCheck, false);
+resetButton.addEventListener("click", reset, false);
